c1964df5f0bc4fda2424b8fd01c1385e1b7e8d48
Maruf Islam <maruf35-2195@diu.edu.bd>
2023-05-08 23:21:42 +0600

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const calculateBMI = () => {
   const weight = document.getElementById('weightInput').value;
-  const height = document.getElementById('heightInput').value / 100; // convert to meters
+  const height = document.getElementById('heightInput').value; // convert to meters
   const bmi = weight / (height * height);
 
   document.getElementById('result').textContent = `Your BMI is ${bmi.toFixed(2)}`;
